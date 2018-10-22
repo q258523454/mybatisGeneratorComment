87658821efaf8c4e47997d9697311ef6330ee752
zhangj <258523454@qq.com>
2018-10-22 16:17:20 +0800

    add: 融资租赁

---
+++ src/main/java/com/dao/Dgpj_Rzzl_Finance_DataMapper.java
@@ -0,0 +1,11 @@
+package com.dao;
+
+import com.entity.Dgpj_Rzzl_Finance_Data;
+
+public interface Dgpj_Rzzl_Finance_DataMapper {
+    /**
+     *
+     * @mbg.generated 2018-10-22
+     */
+    Dgpj_Rzzl_Finance_Data selectByPrimaryKey(Integer table_id);
+}
---
+++ src/main/java/com/entity/Dgpj_Rzzl_Finance_Lease.java
@@ -0,0 +1,487 @@
+package com.entity;
+
+public class Dgpj_Rzzl_Finance_Lease {
+    // 
+    private Integer table_id;
+
+    // 
+    private String apid;
+
+    // 
+    private String leaseIsRight;
+
+    // 
+    private String businessContractNumber;
+
+    // 
+    private String isAreDefense;
+
+    // 
+    private String assetsClassified;
+
+    // 
+    private String outstandingPrincipalBalance;
+
+    // 
+    private String overdueDays;
+
+    // 
+    private String firstReimbursement;
+
+    // 
+    private String irr;
+
+    // 
+    private String leasePeriodMonth;
+
+    // 
+    private String leaseTreatment;
+
+    // 
+    private String serialNumber;
+
+    // 
+    private String feeIncome;
+
+    // 
+    private String settlementCurrency;
+
+    // 
+    private String deductionDeposit;
+
+    // 
+    private String isThirdPartyRights;
+
+    // 
+    private String isRegular;
+
+    // 
+    private String rentPaymentFrequency;
+
+    // 
+    private String riskReserve;
+
+    // 
+    private String leaseExpirationDate;
+
+    // 
+    private String isInvolvedBankruptcyProceedings;
+
+    // 
+    private String totalAmountRent;
+
+    // 
+    private String supplier;
+
+    // 
+    private String rentPaymentDate;
+
+    // 
+    private String aging;
+
+    // 
+    private String customerName;
+
+    // 
+    private String leaseStartDate;
+
+    // 
+    private String outstandingBalanceRent;
+
+    // 
+    private String residualValue;
+
+    // 
+    private String feePayment;
+
+    // 
+    private String lesseeName;
+
+    // 
+    private String interestRate;
+
+    // 
+    private String leaseForm;
+
+    // 
+    private String isFixedRate;
+
+    // 
+    private String rentPayment;
+
+    // 
+    private String depositAmount;
+
+    // 
+    private String isDistinguishBetweenTax;
+
+    // 
+    private String packetDay;
+
+    // 
+    private String pledgeType;
+
+    // 
+    private String isInvolvingStateSecrets;
+
+    // 
+    private String collateralValue;
+
+    // 
+    private String totalAmountPrincipal;
+
+    // 
+    private String lesseeNumber;
+
+    public Integer getTable_id() {
+        return table_id;
+    }
+
+    public void setTable_id(Integer table_id) {
+        this.table_id = table_id;
+    }
+
+    public String getApid() {
+        return apid;
+    }
+
+    public void setApid(String apid) {
+        this.apid = apid;
+    }
+
+    public String getLeaseIsRight() {
+        return leaseIsRight;
+    }
+
+    public void setLeaseIsRight(String leaseIsRight) {
+        this.leaseIsRight = leaseIsRight;
+    }
+
+    public String getBusinessContractNumber() {
+        return businessContractNumber;
+    }
+
+    public void setBusinessContractNumber(String businessContractNumber) {
+        this.businessContractNumber = businessContractNumber;
+    }
+
+    public String getIsAreDefense() {
+        return isAreDefense;
+    }
+
+    public void setIsAreDefense(String isAreDefense) {
+        this.isAreDefense = isAreDefense;
+    }
+
+    public String getAssetsClassified() {
+        return assetsClassified;
+    }
+
+    public void setAssetsClassified(String assetsClassified) {
+        this.assetsClassified = assetsClassified;
+    }
+
+    public String getOutstandingPrincipalBalance() {
+        return outstandingPrincipalBalance;
+    }
+
+    public void setOutstandingPrincipalBalance(String outstandingPrincipalBalance) {
+        this.outstandingPrincipalBalance = outstandingPrincipalBalance;
+    }
+
+    public String getOverdueDays() {
+        return overdueDays;
+    }
+
+    public void setOverdueDays(String overdueDays) {
+        this.overdueDays = overdueDays;
+    }
+
+    public String getFirstReimbursement() {
+        return firstReimbursement;
+    }
+
+    public void setFirstReimbursement(String firstReimbursement) {
+        this.firstReimbursement = firstReimbursement;
+    }
+
+    public String getIrr() {
+        return irr;
+    }
+
+    public void setIrr(String irr) {
+        this.irr = irr;
+    }
+
+    public String getLeasePeriodMonth() {
+        return leasePeriodMonth;
+    }
+
+    public void setLeasePeriodMonth(String leasePeriodMonth) {
+        this.leasePeriodMonth = leasePeriodMonth;
+    }
+
+    public String getLeaseTreatment() {
+        return leaseTreatment;
+    }
+
+    public void setLeaseTreatment(String leaseTreatment) {
+        this.leaseTreatment = leaseTreatment;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getFeeIncome() {
+        return feeIncome;
+    }
+
+    public void setFeeIncome(String feeIncome) {
+        this.feeIncome = feeIncome;
+    }
+
+    public String getSettlementCurrency() {
+        return settlementCurrency;
+    }
+
+    public void setSettlementCurrency(String settlementCurrency) {
+        this.settlementCurrency = settlementCurrency;
+    }
+
+    public String getDeductionDeposit() {
+        return deductionDeposit;
+    }
+
+    public void setDeductionDeposit(String deductionDeposit) {
+        this.deductionDeposit = deductionDeposit;
+    }
+
+    public String getIsThirdPartyRights() {
+        return isThirdPartyRights;
+    }
+
+    public void setIsThirdPartyRights(String isThirdPartyRights) {
+        this.isThirdPartyRights = isThirdPartyRights;
+    }
+
+    public String getIsRegular() {
+        return isRegular;
+    }
+
+    public void setIsRegular(String isRegular) {
+        this.isRegular = isRegular;
+    }
+
+    public String getRentPaymentFrequency() {
+        return rentPaymentFrequency;
+    }
+
+    public void setRentPaymentFrequency(String rentPaymentFrequency) {
+        this.rentPaymentFrequency = rentPaymentFrequency;
+    }
+
+    public String getRiskReserve() {
+        return riskReserve;
+    }
+
+    public void setRiskReserve(String riskReserve) {
+        this.riskReserve = riskReserve;
+    }
+
+    public String getLeaseExpirationDate() {
+        return leaseExpirationDate;
+    }
+
+    public void setLeaseExpirationDate(String leaseExpirationDate) {
+        this.leaseExpirationDate = leaseExpirationDate;
+    }
+
+    public String getIsInvolvedBankruptcyProceedings() {
+        return isInvolvedBankruptcyProceedings;
+    }
+
+    public void setIsInvolvedBankruptcyProceedings(String isInvolvedBankruptcyProceedings) {
+        this.isInvolvedBankruptcyProceedings = isInvolvedBankruptcyProceedings;
+    }
+
+    public String getTotalAmountRent() {
+        return totalAmountRent;
+    }
+
+    public void setTotalAmountRent(String totalAmountRent) {
+        this.totalAmountRent = totalAmountRent;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getRentPaymentDate() {
+        return rentPaymentDate;
+    }
+
+    public void setRentPaymentDate(String rentPaymentDate) {
+        this.rentPaymentDate = rentPaymentDate;
+    }
+
+    public String getAging() {
+        return aging;
+    }
+
+    public void setAging(String aging) {
+        this.aging = aging;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getLeaseStartDate() {
+        return leaseStartDate;
+    }
+
+    public void setLeaseStartDate(String leaseStartDate) {
+        this.leaseStartDate = leaseStartDate;
+    }
+
+    public String getOutstandingBalanceRent() {
+        return outstandingBalanceRent;
+    }
+
+    public void setOutstandingBalanceRent(String outstandingBalanceRent) {
+        this.outstandingBalanceRent = outstandingBalanceRent;
+    }
+
+    public String getResidualValue() {
+        return residualValue;
+    }
+
+    public void setResidualValue(String residualValue) {
+        this.residualValue = residualValue;
+    }
+
+    public String getFeePayment() {
+        return feePayment;
+    }
+
+    public void setFeePayment(String feePayment) {
+        this.feePayment = feePayment;
+    }
+
+    public String getLesseeName() {
+        return lesseeName;
+    }
+
+    public void setLesseeName(String lesseeName) {
+        this.lesseeName = lesseeName;
+    }
+
+    public String getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(String interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public String getLeaseForm() {
+        return leaseForm;
+    }
+
+    public void setLeaseForm(String leaseForm) {
+        this.leaseForm = leaseForm;
+    }
+
+    public String getIsFixedRate() {
+        return isFixedRate;
+    }
+
+    public void setIsFixedRate(String isFixedRate) {
+        this.isFixedRate = isFixedRate;
+    }
+
+    public String getRentPayment() {
+        return rentPayment;
+    }
+
+    public void setRentPayment(String rentPayment) {
+        this.rentPayment = rentPayment;
+    }
+
+    public String getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(String depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+
+    public String getIsDistinguishBetweenTax() {
+        return isDistinguishBetweenTax;
+    }
+
+    public void setIsDistinguishBetweenTax(String isDistinguishBetweenTax) {
+        this.isDistinguishBetweenTax = isDistinguishBetweenTax;
+    }
+
+    public String getPacketDay() {
+        return packetDay;
+    }
+
+    public void setPacketDay(String packetDay) {
+        this.packetDay = packetDay;
+    }
+
+    public String getPledgeType() {
+        return pledgeType;
+    }
+
+    public void setPledgeType(String pledgeType) {
+        this.pledgeType = pledgeType;
+    }
+
+    public String getIsInvolvingStateSecrets() {
+        return isInvolvingStateSecrets;
+    }
+
+    public void setIsInvolvingStateSecrets(String isInvolvingStateSecrets) {
+        this.isInvolvingStateSecrets = isInvolvingStateSecrets;
+    }
+
+    public String getCollateralValue() {
+        return collateralValue;
+    }
+
+    public void setCollateralValue(String collateralValue) {
+        this.collateralValue = collateralValue;
+    }
+
+    public String getTotalAmountPrincipal() {
+        return totalAmountPrincipal;
+    }
+
+    public void setTotalAmountPrincipal(String totalAmountPrincipal) {
+        this.totalAmountPrincipal = totalAmountPrincipal;
+    }
+
+    public String getLesseeNumber() {
+        return lesseeNumber;
+    }
+
+    public void setLesseeNumber(String lesseeNumber) {
+        this.lesseeNumber = lesseeNumber;
+    }
+}
